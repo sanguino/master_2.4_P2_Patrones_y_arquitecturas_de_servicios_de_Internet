a584f2617aa8ff8ee7f5f32a727919917492d7f6
Miguel G. Sanguino <sanguino@me.com>
2021-03-01 17:07:19 +0100

    update cart test

--- src/test/java/es/urjc/code/ejem1/ShoppingCartService.java
+++ src/test/java/es/urjc/code/ejem1/ShoppingCartService.java
@@ -1,39 +1,42 @@
 package es.urjc.code.ejem1;
 
-import es.urjc.code.ejem1.domain.service.ProductServiceImpl;
-import es.urjc.code.ejem1.domain.service.ShoppingCartServiceImpl;
+import es.urjc.code.ejem1.domain.dto.DeletedShoppingCartDTO;
 import es.urjc.code.ejem1.domain.dto.FullProductDTO;
 import es.urjc.code.ejem1.domain.dto.FullShoppingCartDTO;
 import es.urjc.code.ejem1.domain.dto.FullShoppingCartItemDTO;
-import es.urjc.code.ejem1.domain.dto.ProductDTO;
 import es.urjc.code.ejem1.domain.model.Product;
-import es.urjc.code.ejem1.domain.repository.ProductRepository;
-import es.urjc.code.ejem1.domain.repository.ShoppingCartRepository;
-import es.urjc.code.ejem1.service.ValidationServiceImpl;
+import es.urjc.code.ejem1.domain.service.ShoppingCartServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.Random;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class ShoppingCartService {
-	
-	private ProductRepository productRepository;
-	private ProductServiceImpl productService;
 
-	private ShoppingCartRepository shoppingCartRepository;
+	@InjectMocks
 	private ShoppingCartServiceImpl shoppingCartService;
 
+	@Mock
 	private ApplicationEventPublisher applicationEventPublisher;
+
+	@Captor
+	ArgumentCaptor<DeletedShoppingCartDTO> argumentDeletedShoppingCartDTO;
 
 	private ModelMapper mapper = new ModelMapper();
 	
@@ -41,19 +44,14 @@ public class ShoppingCartService {
 	
 	@BeforeEach
 	void setUp() {
-		productRepository = mock(ProductRepository.class);
-		applicationEventPublisher = mock(ApplicationEventPublisher.class);
-		productService = new ProductServiceImpl(applicationEventPublisher);
-		shoppingCartService = new ShoppingCartServiceImpl(
-		        new ValidationServiceImpl(),
-		        applicationEventPublisher);
+		openMocks(this);
 	}
 	
 	@Test
 	@Order(1)
 	void shoppingCartCanBeAdded() {
 		createdShoppingCart = shoppingCartService.createShoppingCart();
-		verify(shoppingCartRepository).save(createdShoppingCart);
+		verify(applicationEventPublisher).publishEvent(createdShoppingCart);
 	}
 	
 	@Test
@@ -63,24 +61,26 @@ public class ShoppingCartService {
 		        "PLUMÍFERO MONTAÑA Y SENDERISMO FORCLAZ TREK100 AZUL CAPUCHA",
 		        "Esta chaqueta acolchada de plumón y plumas, con certificado RDS, abriga bien durante un vivac entre +5 °C y -5 °C.",
 		        49.99);
-		ProductDTO productDTO = mapper.map(product, ProductDTO.class);
+		product.setId(UUID.randomUUID());
+		FullProductDTO fullProductDTO = mapper.map(product, FullProductDTO.class);
 
-		FullProductDTO fullProductDTO = productService.createProduct(productDTO);
-		verify(productRepository).save(fullProductDTO);
-		
+		//when(this.productQueryService.findById(any(String.class))).thenReturn(fullProductDTO);
+
 		int items = Math.abs(new Random().nextInt());
-				
-		createdShoppingCart = shoppingCartService.addProduct(createdShoppingCart, fullProductDTO, items);
-		FullShoppingCartItemDTO fullShoppingCartItemDTO = createdShoppingCart.getItems().get(0);
 
+		createdShoppingCart = shoppingCartService.addProduct(createdShoppingCart, fullProductDTO, items);
+		verify(applicationEventPublisher).publishEvent(any(FullShoppingCartDTO.class));
+		FullShoppingCartItemDTO fullShoppingCartItemDTO = createdShoppingCart.getItems().get(0);
 		assertEquals(fullShoppingCartItemDTO.getQuantity(), items);
-		assertEquals(fullShoppingCartItemDTO.getTotalPrice(), items * productDTO.getPrice());
+		assertEquals(fullShoppingCartItemDTO.getTotalPrice(), items * fullProductDTO.getPrice());
 	}
 	
-	/*@Test
+	@Test
 	@Order(3)
 	void shoppingCartCanBeDeleted() {
-		shoppingCartService.deleteShoppingCart(createdShoppingCart.getId());
-		verify(shoppingCartRepository).deleteById(createdShoppingCart.getId());
-	}*/
+		shoppingCartService.deleteShoppingCart(createdShoppingCart);
+		verify(applicationEventPublisher).publishEvent(this.argumentDeletedShoppingCartDTO.capture());
+		DeletedShoppingCartDTO deletedShoppingCartDTO = this.argumentDeletedShoppingCartDTO.getValue();
+		assertEquals(deletedShoppingCartDTO.getId(), createdShoppingCart.getId());
+	}
 }
