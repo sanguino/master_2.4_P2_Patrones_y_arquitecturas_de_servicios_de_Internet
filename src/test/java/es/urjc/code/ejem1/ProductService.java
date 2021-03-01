7b1547c4fb2051ceec3c59b4060086ed18da1d4b
Miguel G. Sanguino <sanguino@me.com>
2021-03-01 16:47:00 +0100

    updateProduct test

--- src/test/java/es/urjc/code/ejem1/ProductService.java
+++ src/test/java/es/urjc/code/ejem1/ProductService.java
@@ -1,55 +1,68 @@
 package es.urjc.code.ejem1;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
+import es.urjc.code.ejem1.domain.dto.DeletedProductDTO;
+import es.urjc.code.ejem1.domain.dto.FullProductDTO;
+import es.urjc.code.ejem1.domain.dto.ProductDTO;
+import es.urjc.code.ejem1.domain.model.Product;
+import es.urjc.code.ejem1.domain.service.ProductServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
-
-import es.urjc.code.ejem1.domain.dto.FullProductDTO;
-import es.urjc.code.ejem1.domain.model.Product;
-import es.urjc.code.ejem1.domain.dto.ProductDTO;
-import es.urjc.code.ejem1.domain.repository.ProductRepository;
-import es.urjc.code.ejem1.domain.service.ProductServiceImpl;
 import org.springframework.context.ApplicationEventPublisher;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.openMocks;
+
 
 @TestMethodOrder(OrderAnnotation.class)
 public class ProductService {
 
+	@Mock
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	@InjectMocks
 	private ProductServiceImpl productService;
+
+	@Captor
+	ArgumentCaptor<DeletedProductDTO> argumentDeletedProductDTO;
 
 	private ModelMapper mapper = new ModelMapper();
 
 	private static FullProductDTO createdProduct;
 
-	private ApplicationEventPublisher applicationEventPublisher;
-
 	@BeforeEach
 	void setUp() {
-		productService = new ProductServiceImpl(applicationEventPublisher);
+		openMocks(this);
 	}
 
 	@Test
 	@Order(1)
 	void productCanBeAdded() {
 		Product product = new Product(
-		        "PLUMÍFERO MONTAÑA Y SENDERISMO FORCLAZ TREK100 AZUL CAPUCHA",
-		        "Esta chaqueta acolchada de plumón y plumas, con certificado RDS, abriga bien durante un vivac entre +5 °C y -5 °C.",
-		        49.99);
+				"PLUMÍFERO MONTAÑA Y SENDERISMO FORCLAZ TREK100 AZUL CAPUCHA",
+				"Esta chaqueta acolchada de plumón y plumas, con certificado RDS, abriga bien durante un vivac entre +5 °C y -5 °C.",
+				49.99);
 
 		ProductDTO productDTO = mapper.map(product, ProductDTO.class);
 
 		createdProduct = productService.createProduct(productDTO);
+		verify(applicationEventPublisher).publishEvent(createdProduct);
 	}
-/*
+
 	@Test
 	@Order(2)
 	void productCanBeDeleted() {
-		productService.deleteProduct(createdProduct.getId());
-		verify(productRepository).deleteById(createdProduct.getId());
-	}*/
+		productService.deleteProduct(createdProduct);
+		verify(applicationEventPublisher).publishEvent(argumentDeletedProductDTO.capture());
+		DeletedProductDTO deletedProductDTO = this.argumentDeletedProductDTO.getValue();
+		assertEquals(deletedProductDTO.getId(), createdProduct.getId());
+	}
 }
